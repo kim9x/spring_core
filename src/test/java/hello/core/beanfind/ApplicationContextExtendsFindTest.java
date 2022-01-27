975ea54975bca5ded14b278afe5022c5aee96073
Kim9xd <kim9xd@gmail.com>
2022-01-27 21:48:03 +0900

    * 스프링 핵심 원리 이해2 - 객체 지향 원리 적용    - 다양한 설정 형식 지원 - 자바 코드, XML

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.annotation.Testable;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,7 @@ import hello.core.discount.RateDiscountPolicy;
 
 public class ApplicationContextExtendsFindTest {
 	
-	AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+	ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
 	
 	@Test
 	@DisplayName("부모 타입으로 조회 시, 자식이 둘 이상 있으면, 중복 오류가 발생한다.")
